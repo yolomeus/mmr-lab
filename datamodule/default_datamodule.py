fa13e11a3e73f3e2481d9c465348aab9820eb4d2
Fabian <fab.beringer@gmail.com>
2020-11-30 15:25:05 +0100

    add option for collate_fn

--- datamodule/default_datamodule.py
+++ datamodule/default_datamodule.py
@@ -8,15 +8,26 @@ from datamodule import DatasetSplit
 
 class AbstractDefaultDataModule(LightningDataModule):
     """Base class for pytorch-lightning DataModule datasets. Subclass this if you have a standard train, validation,
-    test split."""
+    test split.
+    """
 
-    def __init__(self, train_conf, test_conf, num_workers, pin_memory):
+    def __init__(self, train_conf, test_conf, num_workers, pin_memory, collate_fn_name='collate_fn'):
+        """
+
+        :param train_conf:
+        :param test_conf:
+        :param num_workers:
+        :param pin_memory:
+        :param collate_fn_name:
+        """
         super().__init__()
 
         self._train_conf = train_conf
         self._test_conf = test_conf
         self._num_workers = num_workers
         self._pin_memory = pin_memory
+
+        self._collate_fn_name = collate_fn_name
 
     @property
     @abstractmethod
@@ -43,25 +54,31 @@ class AbstractDefaultDataModule(LightningDataModule):
         """
 
     def train_dataloader(self):
+        collate_fn = getattr(self.train_ds, self._collate_fn_name, None)
         train_dl = DataLoader(self.train_ds,
                               self._train_conf.batch_size,
                               shuffle=True,
                               num_workers=self._num_workers,
-                              pin_memory=self._pin_memory)
+                              pin_memory=self._pin_memory,
+                              collate_fn=collate_fn)
         return train_dl
 
     def val_dataloader(self):
+        collate_fn = getattr(self.val_ds, self._collate_fn_name, None)
         val_dl = DataLoader(self.val_ds,
                             self._test_conf.batch_size,
                             num_workers=self._num_workers,
-                            pin_memory=self._pin_memory)
+                            pin_memory=self._pin_memory,
+                            collate_fn=collate_fn)
         return val_dl
 
     def test_dataloader(self):
+        collate_fn = getattr(self.test_ds, self._collate_fn_name, None)
         test_dl = DataLoader(self.test_ds,
                              self._test_conf.batch_size,
                              num_workers=self._num_workers,
-                             pin_memory=self._pin_memory)
+                             pin_memory=self._pin_memory,
+                             collate_fn=collate_fn)
         return test_dl
 
 
